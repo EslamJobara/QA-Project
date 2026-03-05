76e5f6baa2dce8f474ebd1c08c26886ce3980242
omersakr <omersaqr2001@hotmail.com>
2026-03-06 01:22:36 +0200

    Questions logic & Timer & Background

---
+++ answers.js
@@ -0,0 +1,42 @@
+let container = document.getElementById("answersContainer");
+let scoreBox = document.getElementById("score");
+
+let finalAnswers = JSON.parse(localStorage.getItem("finalAnswers")) || {};
+let subject = "history";
+let examData = {};
+
+fetch("/questions.json")
+  .then((res) => res.json())
+  .then((data) => {
+    examData = data[subject];
+
+    let score = 0;
+
+    examData.forEach((q, i) => {
+      let questionDiv = document.createElement("div");
+      questionDiv.className = "question";
+
+      let title = document.createElement("h3");
+      title.textContent = `Q${i + 1}: ${q.question}`;
+      questionDiv.appendChild(title);
+
+      q.answers.forEach((ans) => {
+        let ansDiv = document.createElement("div");
+        ansDiv.className = "answer";
+        ansDiv.textContent = ans;
+
+        if (ans === q.rightanswer) ansDiv.classList.add("correct");
+        if (finalAnswers[i] === ans) {
+          ansDiv.classList.add("user");
+          if (ans !== q.rightanswer) ansDiv.classList.add("wrong");
+          if (ans === q.rightanswer) score++;
+        }
+
+        questionDiv.appendChild(ansDiv);
+      });
+
+      container.appendChild(questionDiv);
+    });
+
+    scoreBox.textContent = `Score : ${score} / ${examData.length}`;
+  });
